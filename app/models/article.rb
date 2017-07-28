9019d63f185a2489a81bcc3186cea469a29048b4
jaisharma123 <j11798s@gmail.com>
2017-07-28 21:11:04 +0530

    working on article and category with bootstrap

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, uniqueness: true,
+                            length: {minimum: 3}
+  validates :description, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
   get 'home/index'
   root 'home#index'
 
   namespace :admins do
     resources :categories
+    resources :articles
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
